9330744413a9f96d293218c149c771375978d6e6
ludmilaletkova <56293433+ludmilaletkova@users.noreply.github.com>
2020-11-26 02:48:53 +0300

    Update dz2.c

--- dz2.c
+++ dz2.c
@@ -3,7 +3,7 @@
 
 int main(void){
 
-		int m, N, p;
+		int m, N, p, k=0;
 		int i, j;
 		
 
@@ -16,12 +16,22 @@ int main(void){
 			A[i] = (int*)malloc(N * sizeof(int));
 
 
-                //заполнение матрицы в консоле
+        //заполнение матрицы в консоле
 		for (i = 0; i < N; i++)
-			for (j = 0; j < N; j++) 
+			for (j = 0; j < N; j++) {
 				scanf("%d", &A[i][j]);
-			
-
+				if (A[i][j]==1) k++;
+				}
+				FILE* g;
+		g = fopen("g.txt", "w");
+		fprintf(g, "proverka\n");	
+        if (N > 0.5*((k - 2)*(k - 1))) { 
+                             fprintf(g,"svaz\n");
+                         }
+                        else { 
+                             fprintf(g,"ne svaz \n");
+                             }
+							 fclose(g);
 
 		//запись в документ
 		FILE* f;
@@ -32,9 +42,12 @@ int main(void){
 			for (j = 0; j < N; j++) {
 				if (A[i][j] != 0){
                                         while(A[i][j]>=1){
-					fprintf(f, "%d -> %d; \n", i + 1, j + 1);
+
+					   fprintf(f, "%d -> %d; \n", i + 1, j + 1);
                                         A[i][j]--;
                                         }
+						
+
 				}
 			}
 		fprintf(f, "}");
