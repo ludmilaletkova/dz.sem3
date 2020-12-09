7ecaaa218259dfe062b104186c4998b2add617c9
ludmilaletkova <56293433+ludmilaletkova@users.noreply.github.com>
2020-12-09 16:39:14 +0300

    Update dz2.c

--- dz2.c
+++ dz2.c
@@ -3,7 +3,7 @@
 
 int main(void){
 
-		int m, N, p, k=0;
+		int m, N, k = 0;
 		int i, j;
 		
 
@@ -20,12 +20,16 @@ int main(void){
 		for (i = 0; i < N; i++)
 			for (j = 0; j < N; j++) {
 				scanf("%d", &A[i][j]);
-				if (A[i][j]==1) k++;
+				
+				if (A[i][j] == 1)
+				   if (i!= j) 
+				k++;
 				}
+
 				FILE* g;
 		g = fopen("g.txt", "w");
 		fprintf(g, "proverka\n");	
-        if (N > 0.5*((k - 2)*(k - 1))) { 
+        if (k > 0.5*((N - 2)*(N - 1))) { 
                              fprintf(g,"svaz\n");
                          }
                         else { 
